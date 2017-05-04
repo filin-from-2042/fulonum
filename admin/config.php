0551475c14e15d8b9b1426e79e2e56a97e4d7f95
filin-from-2042 <filin2042@gmail.com>
2017-05-04 17:31:08 +0300

    все установлено и руссифицировано

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fulonum.ru/admin/');
+define('HTTP_CATALOG', 'http://fulonum.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fulonum.ru/admin/');
+define('HTTPS_CATALOG', 'http://fulonum.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/fulonum/fulonum.ru/docs/admin/');
+define('DIR_SYSTEM', '/home/fulonum/fulonum.ru/docs/system/');
+define('DIR_IMAGE', '/home/fulonum/fulonum.ru/docs/image/');
+define('DIR_LANGUAGE', '/home/fulonum/fulonum.ru/docs/admin/language/');
+define('DIR_TEMPLATE', '/home/fulonum/fulonum.ru/docs/admin/view/template/');
+define('DIR_CONFIG', '/home/fulonum/fulonum.ru/docs/system/config/');
+define('DIR_CACHE', '/home/fulonum/fulonum.ru/docs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/fulonum/fulonum.ru/docs/system/storage/download/');
+define('DIR_LOGS', '/home/fulonum/fulonum.ru/docs/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/fulonum/fulonum.ru/docs/system/storage/modification/');
+define('DIR_UPLOAD', '/home/fulonum/fulonum.ru/docs/system/storage/upload/');
+define('DIR_CATALOG', '/home/fulonum/fulonum.ru/docs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'fulonum.mysql');
+define('DB_USERNAME', 'fulonum_dev');
+define('DB_PASSWORD', '/3FgfPpC');
+define('DB_DATABASE', 'fulonum_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://fulonum.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://fulonum.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/fulonum/fulonum.ru/docs/catalog/');
+define('DIR_SYSTEM', '/home/fulonum/fulonum.ru/docs/system/');
+define('DIR_IMAGE', '/home/fulonum/fulonum.ru/docs/image/');
+define('DIR_LANGUAGE', '/home/fulonum/fulonum.ru/docs/catalog/language/');
+define('DIR_TEMPLATE', '/home/fulonum/fulonum.ru/docs/catalog/view/theme/');
+define('DIR_CONFIG', '/home/fulonum/fulonum.ru/docs/system/config/');
+define('DIR_CACHE', '/home/fulonum/fulonum.ru/docs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/fulonum/fulonum.ru/docs/system/storage/download/');
+define('DIR_LOGS', '/home/fulonum/fulonum.ru/docs/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/fulonum/fulonum.ru/docs/system/storage/modification/');
+define('DIR_UPLOAD', '/home/fulonum/fulonum.ru/docs/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'fulonum.mysql');
+define('DB_USERNAME', 'fulonum_dev');
+define('DB_PASSWORD', '/3FgfPpC');
+define('DB_DATABASE', 'fulonum_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
